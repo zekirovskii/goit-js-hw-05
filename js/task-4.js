645a3863596b764186348550c87639131b1ce38e
yusuf sengoz <sengozyusuf91@gmail.com>
2025-03-19 23:31:32 +0300

    first commit

---
+++ js/task-4.js
@@ -0,0 +1,48 @@
+const allUsers2 = [
+	{
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+
+const getTotalBalanceByGender = (users, gender) => {
+    return users.reduce((total, user) => {
+        return user.gender === gender ? total + user.balance : total;
+    },0);
+};
+
+console.log(getTotalBalanceByGender(allUsers2, "male")); 
+
+console.log(getTotalBalanceByGender(allUsers2, "female")); 
